5546f1de240340433d9c88d91dfe4570e788e6db
Dragosh Bezhenaru <bezhenarudragosh@gmail.com>
2021-04-01 11:28:46 +0300

    add circle

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -2,7 +2,6 @@ import React, {useEffect, useRef, useState} from 'react';
 import "../style/canvas.scss"
 import {observer} from "mobx-react-lite";
 import CanvasState from '../store/canvasState';
-// import ToolState from '../store/toolState';
 import Brush from '../tools/Brush';
 import toolState from '../store/toolState';
 
@@ -11,7 +10,6 @@ const Canvas = observer(() => {
 
     useEffect(() => {
         CanvasState.setCanvas(canvasRef.current)
-        // ToolState.setTool(new Brush(canvasRef.current))
         toolState.setTool(new Brush(canvasRef.current))
     }, [])
 
--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -3,6 +3,7 @@ import ToolState from '../store/toolState';
 import Brush from '../tools/Brush'
 import '../style/toolbar.scss';
 import Rect from '../tools/Rect';
+import Circle from '../tools/Circle';
 
 const Toolbar = () => {
     return (
@@ -10,7 +11,7 @@ const Toolbar = () => {
             <div className="toolbar__line">
                 <button className="toolbar__btn brush" onClick={() => ToolState.setTool(new Brush(CanvasState.canvas))}/>
                 <button className="toolbar__btn rect" onClick={() => ToolState.setTool(new Rect(CanvasState.canvas))}/>
-                <button className="toolbar__btn circle" />
+                <button className="toolbar__btn circle" onClick={() => ToolState.setTool(new Circle(CanvasState.canvas))}/>
                 <button className="toolbar__btn eraser" />
                 <button className="toolbar__btn line" />
                 <input className="toolbar__color" type="color"/>
