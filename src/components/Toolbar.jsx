1db2c6a21e2e426771aacc8a3f8951e5afb13541
Dragosh Bezhenaru <bezhenarudragosh@gmail.com>
2021-04-01 11:34:01 +0300

    add eraser

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -4,6 +4,7 @@ import Brush from '../tools/Brush'
 import '../style/toolbar.scss';
 import Rect from '../tools/Rect';
 import Circle from '../tools/Circle';
+import Eraser from '../tools/Eraser';
 
 const Toolbar = () => {
     return (
@@ -12,7 +13,7 @@ const Toolbar = () => {
                 <button className="toolbar__btn brush" onClick={() => ToolState.setTool(new Brush(CanvasState.canvas))}/>
                 <button className="toolbar__btn rect" onClick={() => ToolState.setTool(new Rect(CanvasState.canvas))}/>
                 <button className="toolbar__btn circle" onClick={() => ToolState.setTool(new Circle(CanvasState.canvas))}/>
-                <button className="toolbar__btn eraser" />
+                <button className="toolbar__btn eraser" onClick={() => ToolState.setTool(new Eraser(CanvasState.canvas))}/>
                 <button className="toolbar__btn line" />
                 <input className="toolbar__color" type="color"/>
                 <button className="toolbar__btn undo" />
